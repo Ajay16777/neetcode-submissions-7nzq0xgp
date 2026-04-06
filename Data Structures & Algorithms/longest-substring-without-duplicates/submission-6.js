3eee63d560d9972ff129183a92d3b64b07d67e7d
Ajay Singh <64024440+Ajay16777@users.noreply.github.com>
2026-04-06 11:49:12 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const map = new Map();
+        let maxLenght = 0;
+        let left = 0;
+
+        // zxyzxyz. left is z and right is also z
+        // {z: 0, x: 1, y : 1};
+        for (let right = 0; right < s.length; right ++) {
+            if (map.has(s[right]) && map.get(s[right]) >=left) {
+                left = map.get(s[right]) + 1;
+            }
+
+
+            map.set(s[right], right);
+            maxLenght = Math.max(maxLenght, right - left + 1);
+
+            console.log(right), 
+            console.log(left);
+            console.log(s[right]);
+            console.log(s[left])
+                        console.log(map)
+        }
+
+        return maxLenght;
+    }
+}
